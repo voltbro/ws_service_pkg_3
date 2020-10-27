47ac99d0322e8f6d78c5573870445a0c080ef6d1
ramses <slonik256@yandex.ru>
2020-10-27 14:50:43 +0300

    version 2.6.3

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -2,27 +2,46 @@
 
 import time
 import tqdm
+import random
+import os
 
-configuration_number = "0xFF01CA"
-version = "1.1.2"
+configuration_number = "10001"
+
+print(os.system("ls -la"))
+
+configuration_number_1 = "11101010101"
+configuration_number_2 = "00000011100"
+configuration_number_3 = "01111010101"
+configuration_number_4 = "10001110111"
+version = "2.6.3"
 
 print("Service package 3: ver. {}".format(version))
 time.sleep(0.5)
 print("Service package 3: start configuration ")
 time.sleep(0.5)
-print("Service package 3: stage 1")
+print("Service package 3: Search problems stage")
 time.sleep(0.5)
-for i in tqdm.tqdm(range(1000), ascii=True, desc="Finding dependencies"):
-    time.sleep(0.01)
-print("Service package 3: stage 1 finished")
+for i in tqdm.tqdm(range(100), ascii=True, desc="Search log reports"):
+    time.sleep(0.1)
+print("Service package 3: search problems finished")
 time.sleep(0.5)
-print("Service package 3: stage 2")
+print("Service package 3: loading log reports ")
 time.sleep(1.5)
-print("Service package 3: configuration package 1 found")
-for i in tqdm.tqdm(range(5000), ascii=True, desc="Setup in progress"):
-    time.sleep(0.002)
-print("Service package 3: stage 2 finished")
+
+
+
+print(configuration_number_1)
+print(configuration_number_2)
+print(configuration_number_3)
+print(configuration_number_4)
+
+for i in tqdm.tqdm(range(1000), ascii=True, desc="log checking"):
+    time.sleep(0.01)
+
+print("Service package 3: log reports checked successfully")
+
 time.sleep(0.5)
 print("Service package 3: successfully configured!")
+print("Service package 3: finished")
 time.sleep(0.5)
 print("Service package 3: configuration checksum : {}".format(configuration_number))
